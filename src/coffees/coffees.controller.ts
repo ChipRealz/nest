2240cbf4696a5c5e1bb40c7631287c9bd9b7258c
ChipRealz <21110247@student.hcmute.edu.vn>
2024-11-14 20:58:21 +0700

    create coffee service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,19 +1,50 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
-  findAll() {
-    return 'This action returns all coffees';
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findAll(@Query() paginationQuery) {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
+    // return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
+    return this.coffeesService.findOne(id);
+    // return `This action returns #${id} coffee`;
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coffeesService.create(body);
+    // return body;
+    // return `This action creates a coffee`;
+  }
+
+  @Patch(':id')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  update(@Param('id') id: string, @Body() body) {
+    return this.coffeesService.update(id, body);
+    // return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
+    // return `This action removes #${id} coffee`;
   }
 }
